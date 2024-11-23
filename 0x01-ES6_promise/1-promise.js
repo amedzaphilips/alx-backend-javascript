e68b7fbdad69594c1bc143910701e10a715b053e
amedzaphilips <danielamedzaphilips@gmail.com>
2024-11-23 04:20:00 +0000

    true promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -5,7 +5,7 @@ export default function getFullResponseFromAPI(success) {
     if (success) {
       resolve(myObj);
     } else {
-      reject('The fake API is not working currently');
+      reject(new Error('The fake API is not working currently'));
     }
   });
 
