68632d8f67ce199fb9d34d89a4cef81ae397f32f
amedzaphilips <danielamedzaphilips@gmail.com>
2024-12-03 03:55:00 +0000

    get

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -22,7 +22,7 @@ export default class HolbertonCourse {
   set name(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
-      }
+    }
     this._name = value;
   }
 
@@ -31,18 +31,18 @@ export default class HolbertonCourse {
   }
 
   set length(value) {
-     if (typeof value !== 'number') {
+   if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = value;
   }
 
   get students() {
-    return this._students
+    return this._students;
   }
 
   set students(value) {
-     if (!(value instanceof Array)) {
+    if (!(value instanceof Array)) {
       throw new TypeError('Students must be an array of strings');
     }
     if (!value.every((student) => typeof student === 'string')) {
