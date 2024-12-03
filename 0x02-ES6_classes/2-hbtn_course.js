ab5b9af8fab96f787f7df4dcde6b26031302c4e9
amedzaphilips <danielamedzaphilips@gmail.com>
2024-12-03 03:40:40 +0000

    get

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,9 +1,20 @@
+/**
+ * Represents a Holberton Course.
+ */
 export default class HolbertonCourse {
+  /**
+   * Creates a new @see {@link HolbertonCourse}.
+   *
+   * @param {String} name - The name of the course.
+   * @param {Number} length - How long the course is (in months).
+   * @param {String[]} students - The names of students in the course.
+   */
   constructor(name, length, students) {
     this.name = name;
     this.length = length;
     this.students = students;
   }
+
 
   get name() {
     return this._name;
