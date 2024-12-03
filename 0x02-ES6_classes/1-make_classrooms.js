2a01d1340b4c60d6d739486f68bbebba216bb004
amedzaphilips <danielamedzaphilips@gmail.com>
2024-12-03 01:18:59 +0000

    task 1.

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+import ClassRoom from '0-classroom.js';
+
+/**
+ * Creates an array of {@link ClassRoom}s with a specific size.
+ * @returns An array of {@link ClassRoom}s.
+ */
+
+export default function initializeRooms() {
+  const sizes = [19, 20, 34];
+  return sizes.map(size => new ClassRoom(size));
+}
