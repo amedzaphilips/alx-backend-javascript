eb7b7d3e33a6d9d58b8c613366fcbf0d2a1b9ae0
amedzaphilips <danielamedzaphilips@gmail.com>
2024-12-07 01:02:29 +0000

    this 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,5 +1,26 @@
 import Building from '5-building.js'
 
-class SkyHighBuilding() {
+class Building() {
+  constructor(sqft) {
+    if (this.constructor === Building){
+      throw new Error('class is abstract and cannot be instanciated')
+    this._sqft = sqft
+  }
+
+  get sqft() {
+    return this_sqft;
+  }
+
+  set sqft(value) {
+    if (typeof sqft !== number) {
+      throw new Error('value must be a number')
+    } else {
+      this._sqft = value;
+    }
+  }
+
+  evacuationWarningMessage() {
+  throw new Error('Class extending Building must override evacuationWarningMessage')
+  }
 
 }
