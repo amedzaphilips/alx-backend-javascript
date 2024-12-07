89dc8f9c0d1b83c862c807d3d5566eba7889f578
amedzaphilips <danielamedzaphilips@gmail.com>
2024-12-07 11:32:34 +0000

    8

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,29 @@
+export default class ALXClass {
+  constructor(size, location) {
+    this.size =  size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    this._location = value;
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
