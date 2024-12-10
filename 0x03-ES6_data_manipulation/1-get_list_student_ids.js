72e74cbff04ec09aa84cd794b020e31d21f211b6
amedzaphilips <danielamedzaphilips@gmail.com>
2024-12-10 01:44:23 +0000

    task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,6 @@
 export default function getListStudentIds(array) {
   if (!Array.isArray(array)) {
     return [];
-  } else {
-    return array.map((item) => item.id);
   }
+  return array.map((item) => item.id);
 }
