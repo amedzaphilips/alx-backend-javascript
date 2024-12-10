1ab2778fef2715c0341d9c73cbc169762a2855ec
amedzaphilips <danielamedzaphilips@gmail.com>
2024-12-10 02:19:50 +0000

    task 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,5 @@
 export default function getStudentsByLocation(array, city) {
-  if (!Array.isArray(array)){
+  if (!Array.isArray(array)) {
     return [];
   }
   return array.filter((student) => student.location === city);
