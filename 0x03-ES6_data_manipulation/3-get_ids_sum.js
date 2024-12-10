3c2a3db63b33ea59d1a13a3c40c659f9ad644163
amedzaphilips <danielamedzaphilips@gmail.com>
2024-12-10 02:48:03 +0000

    task 2

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,8 @@
 export default function getStudentIdsSum(students) {
   if (students instanceof Array) {
     return students.reduce(
-      (sum, cur) => sum + cur.id, 0);
+      (sum, cur) => sum + cur.id, 0
+    );
   }
   return 0;
 }
