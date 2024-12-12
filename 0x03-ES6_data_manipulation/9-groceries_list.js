7f4995d79778fb6956a0b61c818a4f9c4ad64f67
amedzaphilips <danielamedzaphilips@gmail.com>
2024-12-12 01:54:35 +0000

    map

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,9 @@
 function groceriesList() {
-  return {
+  return new Map({
     Apples: 10,
     Tomatoes: 10,
     Pasta: 1,
     Rice: 1,
     Banana: 5
-  };
+  });
 }
-
-const groceryMap = groceriesList();
